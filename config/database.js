bd963f7525b7c738981523416bc972d80bed397b
MANNEM NANDAPAVANREDDY <103301881+nandapavan26@users.noreply.github.com>
2023-04-19 19:41:11 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     try{
-        const conn = await mongoose.connect("mongodb+srv://nanda_26:nanda@expensetracker.4khhev9.mongodb.net/expensetracker?retryWrites=true&w=majority",{
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true
         });
         console.log(`MongoDB Connected : ${conn.connection.host}`);
